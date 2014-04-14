bc0cd99d08907c363dcfa11a50cbddde85d6c47f
leekyunghee <qkqhlkh@gmail.com>
2014-04-14 16:59:50 +0900

    Update view.js

--- first/src/main/webapp/modules/views/view.js
+++ first/src/main/webapp/modules/views/view.js
@@ -6,7 +6,6 @@ var UserView = Backbone.View.extend({
 	template : _.template($('#user-template').html()),
 	initialize : function() {
 		console.log("UserView initialize");
-		this.listenTo(this.model, 'change', this.render);
 	},
 	render : function() {
 		this.$el.html(this.template(this.model.toJSON()));
@@ -21,33 +20,20 @@ var UserListView = Backbone.View.extend({
 	el: $("#userapp"),
 	initialize : function() {
 		console.log("UserListView initialize");
-		this.listenTo(Users, 'add', this.addOne);
-		this.listenTo(Users, 'reset', this.addAll);
-		this.listenTo(Users, 'all', this.render);
 	},
 	render : function() {
-		//Users.fetch();
-	},
-	addOne : function(user) {
-		console.log("UserListView.addOne");
-		var userView = new UserView({model:user});
-		this.$("#user-list").append(userView.render().el);
-	},
-	addAll : function() {
-		Users.each(this.addOne, this);
+		console.log("EmpListView render()");
+		this.$el.html('');
+		this.collection.refreshData();
+		this.collection.each(function(emp){
+			var userView = new UserView({model: user});
+			this.$el.append(userView.render().el);
+		}, this);
+		return this;
 	},
 	events : {
 		
 	}
 });
 
-var UserList = new UserListView;
-
-console.log("user add");
-var user1 = new User({id: 'idess111', firstName:'yanggon', lastName:'moon', email:'aaa'});
-Users.add(user1);
-var user2 = new User({id: 'idess', firstName:'yanggon', lastName:'moon', email:'aaa'});
-Users.add(user2);
-var user3 = new User({id: 'idess333', firstName:'yanggon', lastName:'moon', email:'aaa'});
-Users.add(user3);
-console.log("user add end");
+var userListView = new UserListView({collection: userCollection});
