cecd615b7c6267c92ed357fc856d574c65f4a0b6
leekyunghee <qkqhlkh@gmail.com>
2014-04-21 09:33:01 +0900

    Update view.js

--- first/src/main/webapp/modules/views/view.js
+++ first/src/main/webapp/modules/views/view.js
@@ -1,59 +1,62 @@
 (function($){
+	/**
+	 * Model 
+	 */
 
-var UserView = Backbone.View.extend({
+	var UserView = Backbone.View.extend({
 	// 타겟 지정 
-	tagName : 'tr',
-	// 템플릿 선언 
-	template : _.template($('#user-template').html()),
-	// 초기화 
-	initialize : function() {
-		console.log("UserView initialize");
-	},
-	render : function() {
-		this.$el.html(this.template(this.model.toJSON())); // 자신의 속성의 값들을 복제 해서 리턴
-		return this;
-	},
-	// 이벤트 핸들러 등록
-	events : {
-	// {'eventName selector': 'callbackFunction'} 형태 
-	}
-});
-
-var UserListView = Backbone.View.extend({
-	el: $("#userapp"),
-	initialize : function() {
-		console.log("UserListView initialize");
-	},
-	render : function() {
-		console.log("EmpListView render()");
-		this.$el.html('');
-		this.collection.refreshData();
-		this.collection.each(function(user){
-			var userView = new UserView({model: user});
-			this.$el.append(userView.render().el);
-		}, this);
-		return this;
-	},
-	events : {
-		
-	}
-});
-
-var userListView = new UserListView({collection: userCollection});
-
-var UserRouter = Backbone.Router.extend({
-	initialize: function(){
-		console.log("UserRouter  initialize()");
-	},
-	routes: {
-		'user/selectUserList': 'selectUserList'
-	},
-	selectEmployeeList: function(){
-		console.log("UserRouter > selectUserList()");
-		userListView.render();
-	}
+		tagName : 'tr',
+		// 템플릿 선언 
+		template : _.template($('#user-template').html()),
+		// 초기화 
+		initialize : function() {
+			console.log("UserView initialize");
+		},
+		render : function() {
+			this.$el.html(this.template(this.model.toJSON())); // 자신의 속성의 값들을 복제 해서 리턴
+			return this;
+		},
+		// 이벤트 핸들러 등록
+		events : {
+		// {'eventName selector': 'callbackFunction'} 형태 
+		}
 	});
 
-	new UserRouter();
-	Backbone.history.start();
+	var UserListView = Backbone.View.extend({
+		el: $("#userapp"),
+		initialize : function() {
+			console.log("UserListView initialize");
+		},
+		render : function() {
+			console.log("EmpListView render()");
+			this.$el.html('');
+			this.collection.refreshData();
+			this.collection.each(function(user){
+				var userView = new UserView({model: user});
+				this.$el.append(userView.render().el);
+			}, this);
+			return this;
+		},
+		events : {
+			
+		}
+	});
+
+	var userListView = new UserListView({collection: userCollection});
+
+	var UserRouter = Backbone.Router.extend({
+		initialize: function(){
+			console.log("UserRouter  initialize()");
+		},
+		routes: {
+			'user/selectUserList': 'selectUserList'
+		},
+		selectEmployeeList: function(){
+			console.log("UserRouter > selectUserList()");
+			userListView.render();
+		}
+		});
+
+		new UserRouter();
+		Backbone.history.start();
 })(jQuery);
